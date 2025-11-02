cde334808b7deaeab31c31d4fc6a0e430dd550ca
sunishae <sunishae44@gmail.com>
2025-11-03 04:02:41 +0900

    feat: 당첨 번호에 대한 클래스 WinningLotto 구현

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.util.LottoErrorMessage;
+
+public class WinningLotto {
+
+    private final Lotto winningNumbers;
+    private final int bonusNumber;
+
+    public WinningLotto(Lotto winningNumbers, int bonusNumber) {
+        validateBonusNumberDuplicate(winningNumbers, bonusNumber);
+
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumberDuplicate(Lotto winningNumbers, int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(LottoErrorMessage.BONUS_NUMBER_DUPLICATE);
+        }
+    }
+
+    public LottoRank match(Lotto userLotto) {
+        int matchCount = userLotto.getMatchCount(winningNumbers);
+        boolean matchBonus = userLotto.contains(bonusNumber);
+
+        return LottoRank.valueOf(matchCount, matchBonus);
+    }
+}
